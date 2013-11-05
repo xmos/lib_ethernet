d3c8db3178d9528be9c5be114569c5491995c5db
David Lacey <david.lacey@xmos.com>
2013-11-05 15:16:35 +0000

    Fix unsafe/safe prototype mismatch

--- module_ethernet/src/mii_filter.h
+++ module_ethernet/src/mii_filter.h
@@ -2,8 +2,8 @@
 #define __mii_filter_h__
 #include "ethernet.h"
 
-void mii_ethernet_filter(const char mac_address[6],
-                         streaming chanend c,
-                         client interface ethernet_filter_if i_filter);
+unsafe void mii_ethernet_filter(const char mac_address[6],
+                                streaming chanend c,
+                                client interface ethernet_filter_if i_filter);
 
 #endif // __mii_filter_h__
