7715b7d059da7a0deade7bcadfe41bfd321c4ad7
Andy Lucas <andy@xmos.com>
2015-03-19 16:07:23 +0000

    Fix issue with etype field wrapping and filter failing

--- lib_ethernet/src/mii_buffering.c
+++ lib_ethernet/src/mii_buffering.c
@@ -20,8 +20,8 @@ extern unsigned increment_and_wrap_power_of_2(unsigned value, unsigned max);
 // There needs to be a block big enough to put the packet header
 // There also needs to be an extra word because the address which
 // is compared is pre-incremented.
-// Need one word extra to ensure dest mac address does not wrap
-#define MIN_USAGE (MII_PACKET_HEADER_BYTES + 4 + 4)
+// Need 3 words extra to ensure dest mac address and ethertype does not wrap
+#define MIN_USAGE (MII_PACKET_HEADER_BYTES + 4 + 12)
 
 hwlock_t ethernet_memory_lock = 0;
 
