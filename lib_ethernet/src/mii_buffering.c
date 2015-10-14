df6f7463015f1d5aa7fd54bf65cccf30b23d7774
Andy Lucas <andy@xmos.com>
2015-10-14 11:35:03 +0100

    Fixes incorrect memset length on packet queue pointers array

--- lib_ethernet/src/mii_buffering.c
+++ lib_ethernet/src/mii_buffering.c
@@ -56,7 +56,7 @@ void mii_init_packet_queue(mii_packet_queue_t queue)
   packet_queue_info_t *info = (packet_queue_info_t *)queue;
   info->rd_index = 0;
   info->wr_index = 0;
-  memset(info->ptrs, 0, ETHERNET_NUM_PACKET_POINTERS);
+  memset(info->ptrs, 0, sizeof(info->ptrs));
 }
 
 mii_mempool_t mii_init_mempool(unsigned *buf, int size)
