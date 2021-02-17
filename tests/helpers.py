44740274a989fc85c6996e2474f5349a3837440d
lucianom <lucianom@xmos.com>
2021-02-17 10:38:05 +0000

    Disable xs1 tests with rgmii

--- tests/helpers.py
+++ tests/helpers.py
@@ -95,7 +95,7 @@ def runall_rx(test_fn, exclude_standard=False):
             test_fn('rt_hp', arch, rx_clk_25, rx_mii, tx_clk_25, tx_mii, seed)
 
     # Test 100 MBit - RGMII
-    for arch in ['xs1', 'xs2']:
+    for arch in ['xs2']:
 
         (rx_clk_25, rx_rgmii) = get_rgmii_rx_clk_phy(Clock.CLK_25MHz, packet_fn=check_received_packet)
         (tx_clk_25, tx_rgmii) = get_rgmii_tx_clk_phy(Clock.CLK_25MHz, verbose=args.verbose)
