c12acebdf16277021916ed1f22fc9a697f0a305b
Ed <ed@xmos.com>
2024-11-06 15:52:32 +0000

    test_timestamp_tx

--- tests/test_timestamp_tx.py
+++ tests/test_timestamp_tx.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python
-# Copyright 2014-2021 XMOS LIMITED.
+# Copyright 2014-2024 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-import xmostest
+import Pyxsim as px
 import os
 import sys
+from pathlib import Path
+import json
+import pytest
+
 from mii_clock import Clock
 from mii_phy import MiiReceiver
 from rgmii_phy import RgmiiTransmitter
 from mii_packet import MiiPacket
-from helpers import get_sim_args, run_on
+from helpers import get_sim_args
 from helpers import get_mii_rx_clk_phy, get_rgmii_rx_clk_phy
 from helpers import get_mii_tx_clk_phy, get_rgmii_tx_clk_phy
+
+with open(Path(__file__).parent / "test_shaper/test_params.json") as f:
+    params = json.load(f)
 
 def packet_checker(packet, phy):
     # Ignore the CRC bytes (-4)
     data = packet.data_bytes[:-4]
 
     if len(data) < 2:
-        print "ERROR: packet doesn't contain enough data ({} bytes)".format(len(data))
+        print(f"ERROR: packet doesn't contain enough data ({len(data)} bytes)")
         return
 
     step = data[1] - data[0]
@@ -26,52 +32,58 @@ def packet_checker(packet, phy):
         x = data[i+1] - data[i]
         x = x & 0xff;
         if x != step:
-            print "ERROR: byte %d is %d more than byte %d (expected %d)." % (i+1, x, i, step)
+            print(f"ERROR: byte {i+1} is {x} more than byte {i} (expected {step}).")
             # Only print one error per packet
             break
 
-def do_test(mac, rx_clk, rx_phy, tx_clk, tx_phy):
-    resources = xmostest.request_resource("xsim")
-
+def do_test(capfd, mac, arch, rx_clk, rx_phy, tx_clk, tx_phy):
     testname = 'test_timestamp_tx'
 
-    binary = '{test}/bin/{mac}_{phy}/{test}_{mac}_{phy}.xe'.format(
-        test=testname, mac=mac, phy=rx_phy.get_name())
+    profile = f'{mac}_{tx_phy.get_name()}'
+    binary = f'{testname}/bin/{profile}/{testname}_{profile}.xe'
+    assert os.path.isfile(binary)
 
-    print "Running {test}: {mac} {phy} phy at {clk}".format(
-        test=testname, mac=mac, phy=rx_phy.get_name(), clk=rx_clk.get_name())
 
-    tester = xmostest.ComparisonTester(open('{test}.expect'.format(test=testname)),
-                                     'lib_ethernet', 'basic_tests', testname,
-                                       {'mac':mac, 'phy':rx_phy.get_name(), 'clk':rx_clk.get_name()},
-                                       regexp=True)
+    with capfd.disabled():
+        print(f"Running {testname}: {mac} {rx_phy.get_name()} phy at {rx_clk.get_name()}")
+
+    tester = px.testers.ComparisonTester(open(f'{testname}.expect'), regexp=True)
 
     simargs = get_sim_args(testname, mac, rx_clk, rx_phy)
-    tester.set_min_testlevel('nightly')
-    xmostest.run_on_simulator(resources['xsim'], binary,
-                              simthreads=[rx_clk, rx_phy, tx_clk, tx_phy],
-                              tester=tester,
-                              simargs=simargs)
 
-def runtest():
+    result = px.run_on_simulator_(  binary,
+                                    simthreads=[rx_clk, rx_phy, tx_clk, tx_phy],
+                                    tester=tester,
+                                    simargs=simargs,
+                                    capfd=capfd,
+                                    do_xe_prebuild=False)
+
+
+@pytest.mark.parametrize("params", params["PROFILES"], ids=["-".join(list(profile.values())) for profile in params["PROFILES"]])
+def test_timestamp_tx(capfd, params):
     # Even though this is a TX-only test, both PHYs are needed in order to drive the mode pins for RGMII
 
-    # Test 100 MBit - MII
-    (rx_clk_25, rx_mii) = get_mii_rx_clk_phy(packet_fn=packet_checker, test_ctrl='tile[0]:XS1_PORT_1C')
-    (tx_clk_25, tx_mii) = get_mii_tx_clk_phy(dut_exit_time=200000)
-    if run_on(phy='mii', clk='25Mhz', mac='rt'):
-        do_test('rt', rx_clk_25, rx_mii, tx_clk_25, tx_mii)
+    verbose = False
 
-    # Test 100 MBit - RGMII
-    (rx_clk_25, rx_rgmii) = get_rgmii_rx_clk_phy(Clock.CLK_25MHz, packet_fn=packet_checker,
-                                                test_ctrl='tile[0]:XS1_PORT_1C')
-    (tx_clk_25, tx_rgmii) = get_rgmii_tx_clk_phy(Clock.CLK_25MHz, dut_exit_time=200000)
-    if run_on(phy='rgmii', clk='25Mhz', mac='rt'):
-        do_test('rt', rx_clk_25, rx_rgmii, tx_clk_25, tx_rgmii)
+    # Test 100 MBit - MII XS2
+    if params["phy"] == "mii":
+        (rx_clk_25, rx_mii) = get_mii_rx_clk_phy(packet_fn=packet_checker, verbose=verbose, test_ctrl='tile[0]:XS1_PORT_1C')
+        (tx_clk_25, tx_mii) = get_mii_tx_clk_phy(dut_exit_time=200000 * 1e6)
+        do_test(capfd, params["mac"], params["arch"], rx_clk_25, rx_mii, tx_clk_25, tx_mii)
 
-    # Test 1000 MBit - RGMII
-    (rx_clk_125, rx_rgmii) = get_rgmii_rx_clk_phy(Clock.CLK_125MHz, packet_fn=packet_checker,
-                                               test_ctrl='tile[0]:XS1_PORT_1C')
-    (tx_clk_125, tx_rgmii) = get_rgmii_tx_clk_phy(Clock.CLK_125MHz, dut_exit_time=300000)
-    if run_on(phy='rgmii', clk='125Mhz', mac='rt'):
-        do_test('rt', rx_clk_125, rx_rgmii, tx_clk_125, tx_rgmii)
+    elif params["phy"] == "rgmii":
+        # Test 100 MBit - RGMII
+        if params["clk"] == "25MHz":
+            (rx_clk_25, rx_rgmii) = get_rgmii_rx_clk_phy(Clock.CLK_25MHz, packet_fn=packet_checker, test_ctrl='tile[0]:XS1_PORT_1C')
+            (tx_clk_25, tx_rgmii) = get_rgmii_tx_clk_phy(Clock.CLK_25MHz, dut_exit_time=200000 * 1e6)
+            do_test(capfd, params["mac"], params["arch"], rx_clk_25, rx_rgmii, tx_clk_25, tx_rgmii)
+        # Test 1000 MBit - RGMII
+        elif params["clk"] == "125MHz":
+            (rx_clk_125, rx_rgmii) = get_rgmii_rx_clk_phy(Clock.CLK_125MHz, packet_fn=packet_checker, verbose=verbose, test_ctrl='tile[0]:XS1_PORT_1C')
+            (tx_clk_125, tx_rgmii) = get_rgmii_tx_clk_phy(Clock.CLK_125MHz, dut_exit_time=300000 * 1e6)
+            do_test(capfd, params["mac"], params["arch"], rx_clk_125, rx_rgmii, tx_clk_125, tx_rgmii)
+        else:
+            assert 0, f"Invalid params: {params}"
+
+    else:
+        assert 0, f"Invalid params: {params}"
