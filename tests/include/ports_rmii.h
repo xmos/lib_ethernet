c46bbc30381b1776704f652c4647b9a51fd9a9ed
Shuchita Khare <shuchitak@xmos.com>
2025-01-09 15:11:37 +0000

    Update copyright (triggered by using checkoutScmShallow() in Jenkinsfile)

--- tests/include/ports_rmii.h
+++ tests/include/ports_rmii.h
@@ -1,4 +1,4 @@
-// Copyright 2024 XMOS LIMITED.
+// Copyright 2024-2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #ifndef __ports_rmii_h__
 #define __ports_rmii_h__
