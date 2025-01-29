d4b45faa8228f87f605130606fc080988438c05d
Ed <ed@xmos.com>
2025-01-29 16:09:59 +0000

    Add passing regex for idle_slope unit

--- tests/test_do_idle_slope_unit.py
+++ tests/test_do_idle_slope_unit.py
@@ -8,12 +8,12 @@ import pytest
 import Pyxsim as px
 
 pkg_dir = Path(__file__).parent
-def test_do_idle_slope(capfd):
-    testname = 'test_do_idle_slope'
+def test_do_idle_slope_unit(capfd):
+    testname = 'test_do_idle_slope_unit'
     binary = pkg_dir / testname / 'bin' / f'{testname}.xe'
     assert os.path.isfile(binary)
     expect_filename = pkg_dir / f'{testname}.expect'
-    tester = px.testers.ComparisonTester(open(expect_filename))
+    tester = px.testers.ComparisonTester(open(expect_filename), regexp=True)
     result = px.run_on_simulator_(  binary,
                                 tester=tester,
                                 do_xe_prebuild=False,
