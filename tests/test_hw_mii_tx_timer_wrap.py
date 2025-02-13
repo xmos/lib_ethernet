0573f9f527c7d943895ad37ba6d2790c353ee324
Shuchita Khare <shuchitak@xmos.com>
2025-02-13 10:14:07 +0000

    Use hw_test_mii_tx_only.xe in the timer wrap test

--- tests/test_hw_mii_tx_timer_wrap.py
+++ tests/test_hw_mii_tx_timer_wrap.py
@@ -44,15 +44,15 @@ def load_packet_file(filename):
 
     return structures
 
-def recv_packet_from_dut(socket_host, xcoreapp, verbose):
+def recv_packet_from_dut(socket_host, xcoreapp, lp_client_id, hp_client_id, verbose):
     expected_packet_len = 1500
     capture_file = "packets.bin"
     socket_host.recv_asynch_start(capture_file)
     time.sleep(SOCKET_RECEIVER_SETUP_TIME) # Wait for the socket receiver to be ready
 
     # now signal to DUT that we are ready to receive and say what we want from it
-    #stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_tx_packets(hp_client_id, 0, 0) # no tx hp
-    stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_tx_packets(0, 1, expected_packet_len)
+    stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_tx_packets(hp_client_id, 0, 0) # no tx hp
+    stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_tx_packets(lp_client_id, 1, expected_packet_len)
 
     host_received_packets = socket_host.recv_asynch_wait_complete()
     if verbose:
@@ -91,7 +91,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
     dut_mac_address_lp = [int(i, 16) for i in dut_mac_address_str_lp.split(":")]
     dut_mac_addres_hp= [int(i, 16) for i in dut_mac_address_str_hp.split(":")]
 
-    xe_name = pkg_dir / "hw_test_mii_tx_loop" / "bin" / "hw_test_mii_tx_loop.xe"
+    xe_name = pkg_dir / "hw_test_mii_tx" / "bin" / "hw_test_mii_tx_only.xe"
     print(f"Asking DUT to send the first packet")
 
     nanoseconds_in_a_second = 1000000000
@@ -107,7 +107,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
         lp_client_id = 0
         hp_client_id = 1
         stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_macaddr(lp_client_id, dut_mac_address_str_lp)
-        #xcoreapp.xscope_controller_cmd_set_dut_macaddr(hp_client_id, dut_mac_address_str_hp)
+        stdout, stderr = xcoreapp.xscope_controller_cmd_set_dut_macaddr(hp_client_id, dut_mac_address_str_hp)
         stdout, stderr = xcoreapp.xscope_controller_cmd_set_host_macaddr(host_mac_address_str)
 
         print("Starting sniffer")
@@ -115,7 +115,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
             assert platform.system() in ["Linux"], f"Receiving using sockets only supported on Linux"
             socket_host = SocketHost(eth_intf, host_mac_address_str, f"{dut_mac_address_str_lp} {dut_mac_address_str_hp}")
 
-            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, verbose)
+            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, lp_client_id, hp_client_id, verbose)
             packet_recv_times.append((tv_sec, tv_nsec))
             if verbose:
                 print(f"recv time = {packet_recv_times[-1][0] + (packet_recv_times[-1][1]*nanoseconds_in_a_second)} ns")
@@ -127,7 +127,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
                 print(f"wait_time = {wait_times_s[-1]} s")
             time.sleep(wait_times_s[-1])
 
-            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, verbose)
+            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, lp_client_id, hp_client_id, verbose)
             packet_recv_times.append((tv_sec, tv_nsec))
             if verbose:
                 print(f"recv time = {packet_recv_times[-1][0] + (packet_recv_times[-1][1]*nanoseconds_in_a_second)} ns")
@@ -138,7 +138,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
                 print(f"wait_time = {wait_times_s[-1]} s")
             time.sleep(wait_times_s[-1])
 
-            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, verbose)
+            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, lp_client_id, hp_client_id, verbose)
             packet_recv_times.append((tv_sec, tv_nsec))
             if verbose:
                 print(f"recv time = {packet_recv_times[-1][0] + (packet_recv_times[-1][1]*nanoseconds_in_a_second)} ns")
@@ -149,7 +149,7 @@ def test_hw_mii_tx_timer_wrap(request, send_method):
                 print(f"wait_time = {wait_times_s[-1]} s")
             time.sleep(wait_times_s[-1])
 
-            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, verbose)
+            tv_sec, tv_nsec = recv_packet_from_dut(socket_host, xcoreapp, lp_client_id, hp_client_id, verbose)
             packet_recv_times.append((tv_sec, tv_nsec))
             if verbose:
                 print(f"recv time = {packet_recv_times[-1][0] + (packet_recv_times[-1][1]*nanoseconds_in_a_second)} ns")
