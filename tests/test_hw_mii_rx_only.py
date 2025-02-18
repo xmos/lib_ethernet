7a427dbc9e888b78aae93cc4587f109f7fb54eb0
Shuchita Khare <shuchitak@xmos.com>
2025-02-18 12:23:31 +0000

    Add extra waits in test_hw_mii_rx_only to (a.) allow debugger phy link up and (b.) allow all packets to drain out of the debugger before terminating xcore app

--- tests/test_hw_mii_rx_only.py
+++ tests/test_hw_mii_rx_only.py
@@ -122,9 +122,11 @@ def test_hw_mii_rx_only(request, send_method, payload_len):
 
             time.sleep(sleep_time + 10) # Add an extra 10s of buffer
         elif send_method == "socket":
+            time.sleep(5) # To allow link up on the debugger phys. TODO
             num_packets_sent = socket_host.send(test_duration_s, payload_len=payload_len)
 
         print("Retrive status and shutdown DUT")
+        time.sleep(0.1) # To allow all packets to be sent out of the debugger before terminating the xcore app. TODO
         stdout, stderr = xcoreapp.xscope_controller_cmd_shutdown()
 
         print("Terminating!!!")
