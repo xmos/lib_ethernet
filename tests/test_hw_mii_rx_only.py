25f8d712433eb75bc57d39efe8000d025e1f4585
Shuchita Khare <shuchitak@xmos.com>
2025-02-19 15:07:10 +0000

    Added code to autogenerate xscope cmds enum .h file in CMakeLists.txt of the hw test apps

--- tests/test_hw_mii_rx_only.py
+++ tests/test_hw_mii_rx_only.py
@@ -9,8 +9,8 @@ from hw_helpers import mii2scapy, scapy2mii, get_mac_address
 import pytest
 from contextlib import nullcontext
 import time
-from xcore_app_control import XcoreAppControl, SocketHost
-from xcore_app_control import scapy_send_l2_pkts_loop, scapy_send_l2_pkt_sequence
+from xscope_host import XcoreAppControl
+from socket_host import SocketHost, scapy_send_l2_pkts_loop, scapy_send_l2_pkt_sequence
 import re
 import subprocess
 import platform
