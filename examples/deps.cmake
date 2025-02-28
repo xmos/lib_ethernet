e2d1c9c597f7dc8028991161af75e21e72f05360
Ed <ed@xmos.com>
2025-02-28 15:46:51 +0000

    Update deps.cmake

--- examples/deps.cmake
+++ examples/deps.cmake
@@ -1,8 +1,4 @@
 set(APP_DEPENDENT_MODULES   lib_ethernet
                             "lib_otpinfo(feature/xccm)"
-<<<<<<< HEAD
-                            "lib_board_support(feature/xk_eth_xu316_dual_100m)"
-=======
                             "shuchitak/lib_board_support(sync_link_state_with_mac)"
->>>>>>> hw_test
                             )
