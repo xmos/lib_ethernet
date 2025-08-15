3d2877ee1f98e46d856ef797e4eb71cde4031c8f
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-08-15 15:38:17 +0100

    Temporary, Pointing to board_support fork for testing

--- examples/deps.cmake
+++ examples/deps.cmake
@@ -1,2 +1,2 @@
 set(APP_DEPENDENT_MODULES   lib_ethernet
-                            "lib_board_support(develop)") # Planning for 1.4.0
+                            "humphrey-xmos/lib_board_support(adding-eth-audio-xu316)") # Planning for 1.4.0
