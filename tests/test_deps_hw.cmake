ac1039360d55fa05bcdd8fe8b376b008a766ed4b
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-08-15 16:25:13 +0100

    Fixing hw test deps

---
+++ tests/test_deps_hw.cmake
@@ -0,0 +1,3 @@
+set(APP_DEPENDENT_MODULES   lib_ethernet
+                            "lib_otpinfo(2.2.1)"
+                            "humphrey-xmos/lib_board_support(adding-eth-audio-xu316)") # Planning for 1.4.0
