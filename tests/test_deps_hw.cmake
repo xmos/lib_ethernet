8579c9bf31c3398bb9a1346830259ceb60ea02a2
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-08-19 12:37:30 +0100

    Dependency back to develop

--- tests/test_deps_hw.cmake
+++ tests/test_deps_hw.cmake
@@ -1,3 +1,3 @@
 set(APP_DEPENDENT_MODULES   lib_ethernet
                             "lib_otpinfo(2.2.1)"
-                            "humphrey-xmos/lib_board_support(adding-eth-audio-xu316)") # Planning for 1.4.0
+                            "lib_board_support(develop)") # Planning for 1.4.0
